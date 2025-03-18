7bc4dcec8216e7c550a4d7a05e56c2ed12a069ae
prasad-bigdp <bigdp585@gmail.com>
2025-03-18 16:17:41 +0530

    update

--- code/directivesDemo1/src/app/app.component.ts
+++ code/directivesDemo1/src/app/app.component.ts
@@ -7,14 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  isShow = false;
-  user="parent"
-  fun() {
-    if (this.isShow == true) {
-      this.isShow = false;
-    } else {
-      this.isShow = true;
-    }
-  }
-  isSigned = false;
+  arr: string[] = ['raj', 'PRASAD', 'shekhar', 'manu', 'vani'];
+  movies: any[] = [
+    {
+      title: 'RRR',
+      link: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/d7/RRR_Poster.jpg/220px-RRR_Poster.jpg',
+    },
+    {
+      title: 'salaar',
+      link: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/d7/RRR_Poster.jpg/220px-RRR_Poster.jpg',
+    },
+    {
+      title: 'Pushpa',
+      link: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/d7/RRR_Poster.jpg/220px-RRR_Poster.jpg',
+    },
+  ];
+  arr2 = [
+    { name: "prasad", role: "deloper" },
+    { name: "Jaanu", role: "HR" },
+    {name:"angel priya", role:"scamming people"}
+]
+  // isShow = false;
+  // user = 'parent';
+  // fun() {
+  //   if (this.isShow == true) {
+  //     this.isShow = false;
+  //   } else {
+  //     this.isShow = true;
+  //   }
+  // }
+  // isSigned = false;
 }
