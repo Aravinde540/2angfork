1e300e572463200c6c6105ae524a53ccc1961fbc
prasad-bigdp <bigdp585@gmail.com>
2025-04-03 15:55:54 +0530

    update

--- movieMania/src/app/app.component.spec.ts
+++ movieMania/src/app/app.component.spec.ts
@@ -1,16 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterModule.forRoot([])
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,11 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
- 
+  it(`should have the 'movieMania' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('movieMania');
+  });
+
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, portfolio');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, movieMania');
   });
 });
